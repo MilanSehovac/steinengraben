d33f8a230db30136452f8cc7e6000f939db7baaf
MilanSehovac <milansehovacx5@gmail.com>
2022-07-29 14:17:08 +0200

    s

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+
 function video(){
     document.querySelector('.section-d-vid-placeholder').classList.add("d-none")
     document.querySelector('.play-icon').classList.add("d-none")
     document.querySelector('.section-d-yt-video').classList.remove("d-none")
+}
+function expand(){
+    document.querySelector('.new-table1-row1').classList.toggle("d-none")
 }
 window.addEventListener("hashchange", () => window.history.pushState({}, "", '/steinengraben/index.html'), {});
