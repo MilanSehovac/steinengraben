a97d9fbe4e1bf52c21c129fd8a674793398d2c5c
MilanSehovac <milansehovacx5@gmail.com>
2022-08-01 14:03:57 +0200

    s

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\r\nfunction video(){\r\n    document.querySelector('.section-d-vid-placeholder').classList.add(\"d-none\")\r\n    document.querySelector('.play-icon').classList.add(\"d-none\")\r\n    document.querySelector('.section-d-yt-video').classList.remove(\"d-none\")\r\n}\r\nfunction expand(){\r\n    document.querySelector('.new-table1-row1').classList.toggle(\"d-none\")\r\n}\r\nfunction expand2(){\r\n    document.querySelector('.new-table1-row2').classList.toggle(\"d-none\")\r\n}\r\nwindow.addEventListener(\"hashchange\", () => window.history.pushState({}, \"\", '/steinengraben/index.html'), {});\n\n//# sourceURL=webpack://steinengraben/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <h1>basd</h1>,
+    document.querySelector('#fromjs')
+)
