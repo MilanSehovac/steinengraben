31376ebcef60cae53f8267ebc1970dcd83f9477a
MilanSehovac <milansehovacx5@gmail.com>
2022-08-03 10:12:43 +0200

    s

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-y = 0
-x = 0
+plusIconTwo = 0
+plusIconOne = 0
 
 function video(){
     document.querySelector('.section-d-vid-placeholder').classList.add("d-none")
@@ -10,24 +10,24 @@ function video(){
 function expand(){
     document.querySelector('.new-table1-row1').classList.toggle("d-none")
     document.querySelector('.expander1').style.backgroundImage="url(assets/minus-icons-opacity.png)"
-    if (x == 1){
+    if (plusIconOne == 1){
         document.querySelector('.expander1').style.backgroundImage="url(assets/plus-icons-opacity.png)"
-        x = 0
+        plusIconOne = 0
     }
     else{
-        x = 1;
+        plusIconOne = 1;
     }
 
 }
 function expand2(){
     document.querySelector('.new-table1-row2').classList.toggle("d-none")
     document.querySelector('.expander2').style.backgroundImage="url(assets/minus-icons-opacity.png)"
-    if (y == 1){
+    if (plusIconTwo == 1){
         document.querySelector('.expander2').style.backgroundImage="url(assets/plus-icons-opacity.png)"
-        y = 0
+        plusIconTwo = 0
     }
     else{
-        y = 1;
+        plusIconTwo = 1;
     }
 }
 window.addEventListener("hashchange", () => window.history.pushState({}, "", '/steinengraben/index.html'), {});
